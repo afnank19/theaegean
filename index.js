310820c163f31a1446de0cfe370ffef23c40b6bd
Afnan Khalid <afnank19@gmail.com>
2024-10-08 19:47:52 +0500

    init express app

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Future server");
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({ respone: "wow" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up on http://localhost:${PORT}`);
+});
